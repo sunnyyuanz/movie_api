3c3d2fafbd19c1043ca6677c366f8d8729ea9474
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-07-20 17:27:08 -0400

    mongodb and mongoose

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express'),
-  morgan = require('morgan'),
   fs = require('fs'),
   path = require('path'),
   bodyParser = require('body-parser'),
   uuid = require('uuid');
 
+const morgan = require('morgan');
 const app = express();
+const mongoose = require('mongoose');
+const model = require('./model.js');
+
+const movies = model.Movie;
+const users = model.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', (err) => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!!!');
+});
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a',
@@ -14,117 +24,210 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
 app.use(morgan('combined', { stream: accessLogStream }));
 app.use(bodyParser.json());
 
-let collections = [];
-let users = [];
-let movies = [
-  {
-    id: 1,
-    title: "Harry Potter and the Sorcerer's Stone",
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-  {
-    id: 2,
-    title: 'Harry Potter and the Chamber of Secrets',
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-  {
-    id: 3,
-    title: 'Harry Potter and the Prisoner of Azkaban',
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-  {
-    id: 4,
-    title: 'Harry Potter and the Goblet of Fire',
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-  {
-    id: 5,
-    title: 'Harry Potter and the Order of the Phoenix',
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-  {
-    id: 6,
-    title: 'Harry Potter and the Half-Blood Prince',
-    Description:
-      "Harry Potter and the Philosopher's Stone (released in the United States, India and the Philippines as Harry Potter and the Sorcerer's Stone) is a 2001 fantasy film directed by Chris Columbus and distributed by Warner Bros. Pictures, based on J. K. Rowling's 1997 novel of the same name. Produced by David Heyman and screenplay by Steve Kloves, it is the first instalment of the Harry Potter film series. The film stars Daniel Radcliffe as Harry Potter, with Rupert Grint as Ron Weasley, and Emma Watson as Hermione Granger. Its story follows Harry's first year at Hogwarts School of Witchcraft and Wizardry as he discovers that he is a famous wizard and begins his formal wizarding education.",
-    genre: "fantasy, Adventure, Children's film, Narrative",
-    director: 'Chris Columbus',
-    imageurl: '',
-  },
-];
-
-app.get('/movies', (req, res) => {
-  res.json(movies);
+app.get('/', (req, res) => {
+  res.send('Welcome to MyFlix!');
 });
 
-app.get('/movies/:title', (req, res) => {
-  res.json(
-    movies.find((movie) => {
-      return movie.title === req.params.title;
+//get all movies
+app.get('/movies', (req, res) => {
+  movies
+    .find()
+    .then((movies) => {
+      res.status(201).json(movies);
     })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+
+//get movies/movie by its movie title
+app.get('/movies/:title', (req, res) => {
+  movies
+    .find({ Title: req.params.title })
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+
+//get genres by genre name
+app.get('/movies/genres/:name', (req, res) => {
+  movies
+    .findOne({ 'Genre.Name': req.params.name })
+    .then((movie) => {
+      res.json(movie.Genre);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+
+//get directors by director name
+app.get('/movies/directors/:directorname', (req, res) => {
+  movies
+    .findOne({ 'Director.Name': req.params.directorname })
+    .then((movie) => {
+      res.json(movie.Director);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+//update a user's info, by username
+// we'll expect JSON in this format
+// {
+//   Username:String,(require)
+//   Password:String,(require)
+//   Email:String,(require)
+//   Birthday:Date
+// }
+app.put('/users/:Username', (req, res) => {
+  users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }, //This line makes sure that the updated document is returned
+    (err, updateUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+      } else {
+        res.json(updateUser);
+      }
+    }
   );
 });
 
-app.get('/movies/genres/:title', (req, res) => {
-  res.json(movies[1].genre);
-});
-
-app.get('/movies/directors/:directorname', (req, res) => {
-  res.send('Chris Columbus');
-});
-
-app.put('/users/:username', (req, res) => {
-  res.status(500).send('Feature is not available now!');
-});
-
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-  newUser.id = uuid.v4();
-  users.push(newUser);
-  return res.status(201).send(newUser);
-});
-
-app.delete('/users/:id', (req, res) => {
-  res.status(201).send('User is deleted.');
-});
-
-app.post('/movies/collections', (req, res) => {
-  let newMovie = req.body;
-  newMovie.id = uuid.v4();
-  collections.push(newMovie);
-  return res.status(201).send(newMovie);
-});
-
-app.delete('/movies/collections/:title', (req, res) => {
-  let movie = movies.find((movie) => {
-    return movie.title === req.params.title;
-  });
-  if (movie) {
-    movies = movies.filter((obj) => {
-      return obj.title !== req.params.title;
+//Get all users
+app.get('/users', (req, res) => {
+  users
+    .find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error:' + err);
     });
-    res.status(201).send('movie' + req.params.title + 'was deleted.');
-  }
+});
+
+//Get a user by username
+app.get('/users/:Username', (req, res) => {
+  users
+    .findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+
+//Add a user
+// we'll expect JSON in this format
+// {
+//   ID:Integer,
+//   Username:String,
+//   Password:String,
+//   Email:String,
+//   Birthday:Date
+
+// }
+app.post('/users', (req, res) => {
+  users
+    .findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
+          })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error:' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error:' + error);
+    });
+});
+
+//Delete/Deregister a user by username
+app.delete('/users/:Username', (req, res) => {
+  users
+    .findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error:' + err);
+    });
+});
+
+//Add a movie to a user's list of favorites
+app.post('/users/:username/collections/:MovieID', (req, res) => {
+  users.findOneAndUpdate(
+    { Username: req.params.username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, //This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+//Remove a movie from user's list of favorites
+app.delete('/users/:username/collections/:MovieID', (req, res) => {
+  users.findOneAndUpdate(
+    { Username: req.params.username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, //This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error:' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
 });
 
 app.get('/documentation', (req, res) => {
