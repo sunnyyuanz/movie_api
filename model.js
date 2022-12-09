7ec84273dda5e386de5a537873cda7c2c9052b0d
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2022-12-09 12:17:12 -0500

    switch back to heroku

--- model.js
+++ model.js
@@ -18,9 +18,9 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-  Username: { type: String, required: false },
+  Username: { type: String, required: true },
   Password: { type: String, required: true },
-  Email: { type: String, required: true },
+  Email: { type: String, required: false },
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
